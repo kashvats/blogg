0e9d51fe78c9a8cb47d265789038118d9f284616
aakash <vatsakash168@gmail.com>
2021-10-29 11:33:41 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -143,8 +143,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testm9478@gmail.com'
-EMAIL_HOST_PASSWORD = 'akash@123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 # EMAIL_TIMEOUT= 500
 
