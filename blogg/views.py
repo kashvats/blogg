9def18a95ae25f9ecb2a86bc10fb3804cf46ab53
aakash <vatsakash168@gmail.com>
2023-07-10 12:30:47 +0530

    up

--- blogg/views.py
+++ blogg/views.py
@@ -14,7 +14,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 
 # user registration form
-
+#this is merge
 
 def user_register(request):
     if request.method == 'POST':
